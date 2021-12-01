5f777f68b4072633aaca2b7eb2b18d93e378d3a6
raulito28 <raulbuenoherrera@gmail.com>
2021-12-01 11:38:03 +0100

    Update

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 lista_numeros = []
 while True:
-    try:
-        e = int(input("Introducir elemento de lista a ordenar: "))
-        
+    while True:
+        try:
+            e = int(input("Introducir elemento de lista a ordenar: "))
+            break
+        except:
+            print("El dato introducido no es un número que sea válido")
+
+    if (e == -9999):
+        break
+    lista_numeros.append(e)
+    n = len(lista_numeros)
+    for i in range(n-1):
